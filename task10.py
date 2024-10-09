c9cac74ac4ab538a1aa89dc0ef39ef8626eee794
Conor O'Dwyer <c.odwyer17@universityofgalway.ie>
2024-10-09 16:27:00 +0100

    copied task 9 to task 10

--- task10.py
+++ task10.py
@@ -1,3 +1,29 @@
 import csv
 import matplotlib.pyplot as plt
 from datetime import datetime
+
+dates = []
+prices = {'META': [], 'AAPL': [], 'AMZN': [], 'NFLX': [], 'NVDA': [], 'GOOGL': []}
+colors = {'META': 'blue', 'AAPL': 'gray', 'AMZN': 'black', 'NFLX': 'red', 'NVDA': 'green', 'GOOGL': 'orange'}
+percentage_changes = {}
+
+with open("task9.csv", "r") as file:
+ reader = csv.reader(file)
+ next(reader)
+ for row in reader:
+  dates.append(datetime.strptime(row[0], '%d/%m/%Y'))
+
+  for i, stock in enumerate(prices.keys(), start=1):
+   prices[stock].append(float(row[i]))
+
+for stock, stock_prices in prices.items():
+ plt.plot(dates, stock_prices, label=stock)
+
+plt.title("Stock Prices Over Time")
+plt.xlabel("Date")
+plt.ylabel("Price (USD)")
+plt.legend()
+plt.grid(True)
+plt.gcf().autofmt_xdate()
+plt.show()
+
